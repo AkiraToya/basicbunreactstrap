0cc8fe3c704cd8223f3c1d513e76caa1bce9aeed
Jarvis <akira_toya2006@yahoo.com>
2024-05-01 09:05:40 +0700

    Use variable, Row, Col, and css styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import { Card, CardBody, Container } from "reactstrap";
+import { Button, Card, CardBody, Col, Container, Row } from "reactstrap";
 
 function App() {
+  let count = 0
+
   return (
     <Container>
       <Card className="margin-top-20">
         <CardBody className="center">
-          Hello World
+          <Row>
+            <Col style={{
+              fontSize: "1.5em"
+            }}>
+              Count: {count}
+            </Col>
+          </Row>
+          <br />
+          <Row>
+            <Col>
+              <Button className="full-width">-</Button>
+            </Col>
+            <Col>
+              <Button className="full-width">+</Button>
+            </Col>
+          </Row>
         </CardBody>
       </Card>
     </Container>
